c9ddb8a44f67c48ef31dbe16b55e72c1c853591d
alladisriya <sriyaalladi@gmail.com>
2020-02-07 18:23:23 +0530

    firsttask

---
+++ hometask/src/main/java/com/epam/hometask/Chocolate.java
@@ -0,0 +1,13 @@
+package com.epam.hometask;
+
+
+public class Chocolate extends Sweets{
+String type;
+Chocolate(int n, int ct, int wt, String type){
+	super(n,ct,wt);
+	this.type = type;
+}
+public String toString() {
+	return "Cost: "+this.ct+" Weight: "+this.wt+" Total Number: "+this.n;
+}
+}
